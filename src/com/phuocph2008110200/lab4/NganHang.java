9ef022e2c2b7a8530a92a80eb313ecd0be0feb6f
Phanphuoc558 <phanphuocpro430@gmail.com>
2021-10-07 07:59:51 +0700

    Cập Nhật Lần 3

--- src/com/phuocph2008110200/lab4/NganHang.java
+++ src/com/phuocph2008110200/lab4/NganHang.java
@@ -10,13 +10,49 @@ public class NganHang {
         public static void main(String[] args) {
                 Account account = new Account();
                 Scanner in = new Scanner(System.in);
-                System.out.print("Nhập vào số tiền gửi: ");
-                int x = in.nextInt();
-                account.deposit(x);
-                account.showData();
-                System.out.print("Nhập vào số tiền rút: ");
-                int y = in.nextInt();
-                account.withdraw(y);
-                account.showData();
-        }
+                int chose;
+                do{
+                        System.out.println("***************MENU BANK*****************");
+                        System.out.println("| 1. Rút, gửi, xem thông tin tài khoản. |");
+                        System.out.println("| 2. Nhân viên.                         |");
+                        System.out.println("| 3. Khách hàng.                        |");
+                        System.out.println("| Nhập số bất kì để kết thúc.           |");
+                        System.out.println("*****************************************");
+                        System.out.print("Nhập chức năng cần thực hiện: ");
+                        chose = in.nextInt();
+                        switch(chose){
+                                case 1:{
+                                        int chose1;
+                                        do{
+                                                System.out.println("****************MENU ACCOUNT*************");
+                                                System.out.println("| 1. Rút tiền.                          |");
+                                                System.out.println("| 2. Gửi tiền.                          |");
+                                                System.out.println("| 3. Xem thông tin tài khoản.           |");
+                                                System.out.println("| Nhập số bất kì để kết thúc.           |");
+                                                System.out.println("*****************************************");
+                                                System.out.print("Nhập chức năng cần thực hiện: ");
+                                                chose1 = in.nextInt();
+                                                switch(chose1){
+                                                        case 1:{
+                                                                System.out.print("Nhập vào số tiền rút: ");
+                                                                int b = in.nextInt();
+                                                                account.withdraw(b);
+                                                        };break;
+                                                        case 2:{
+                                                                System.out.print("Nhập vào số tiền gửi: ");
+                                                                int a = in.nextInt();
+                                                                account.deposit(a);
+                                                        };break;
+                                                        case 3:{
+                                                                account.showData();
+                                                                System.out.print("Nhập số bất kì để quay về menu sau khi kiểm tra kỹ thông tin.");
+                                                                int i = in.nextInt();
+                                                        }
+                                                }  
+                                        }while(chose1 > 0 && chose1 < 4);
+                                }
+                        }
+                        
+                }while(chose > 0 && chose < 4);
+}
 }

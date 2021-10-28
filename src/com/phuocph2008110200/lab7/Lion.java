972ef07331ff10e6ca58150666cb68a90d643f64
Phanphuoc558 <phanphuocpro430@gmail.com>
2021-10-28 07:28:35 +0700

    Lần 2

---
+++ src/com/phuocph2008110200/lab7/Lion.java
@@ -0,0 +1,12 @@
+/*
+# Ngày Tạo: 28 Oct 2021
+# Tác Giả: Phan Hửu Phước
+*/
+package com.phuocph2008110200.lab7;
+
+public class Lion extends Claws{
+    @Override
+    protected void makeNoise() {
+        System.out.println("Tiếng gầm của sư tử");
+    }
+}
---
+++ src/com/phuocph2008110200/lab7/Tiger.java
@@ -0,0 +1,12 @@
+/*
+# Ngày Tạo: 28 Oct 2021
+# Tác Giả: Phan Hửu Phước
+*/
+package com.phuocph2008110200.lab7;
+
+public class Tiger extends Claws{
+    @Override
+    protected void makeNoise() {
+        System.out.println("Tiếng gầm của hổ");
+    }
+}

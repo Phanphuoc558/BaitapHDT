33a689e3572ecd0afdd39ff3661067bc24d9ec5b
Phanphuoc558 <phanphuocpro430@gmail.com>
2021-11-04 09:10:53 +0700

    đổii thêm xí thầy ơi <3

--- src/com/phuocph2008110200/kiemtragiuaky/HocVien.java
+++ src/com/phuocph2008110200/kiemtragiuaky/HocVien.java
@@ -15,14 +15,16 @@ public class HocVien extends Nguoi{
         return super.toString()+" "+diemMonhoc1+" "+diemMonhoc2+" "+" Diem trung binh "+diemTB();
     }
     public void setDiemMonhoc1() {
-        System.out.println("Nhap vao diem mon 1: ");
+       
         do{
+            System.out.println("Nhap vao diem mon 1: ");
             diemMonhoc1=in.nextFloat();
         }while(diemMonhoc1<0&&diemMonhoc1>10);
     }
     public void setDiemMonhoc2() {
-        System.out.println("Nhap vao diem mon 2: ");
+        
         do{
+            System.out.println("Nhap vao diem mon 2: ");
             diemMonhoc2=in.nextFloat();
         }while(diemMonhoc2<0&&diemMonhoc2>10);
     }
